c6698b0a066e759441986eb16df30fa6ecba68e2
Nurislam <satybaldievnurislam248@gmail.com>
2024-02-25 22:44:31 +0600

    Lab5

--- Lab3OOP/Lab3OOP/Lab3OOP.cpp
+++ Lab3OOP/Lab3OOP/Lab3OOP.cpp
@@ -1,92 +1,92 @@
 ﻿//////1 задание
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
-#include <iomanip>
-
-using namespace std;
-
-class Timer {
-private:
-    int seconds;
-
-public:
-    // Конструкторы
-    Timer(int initialSeconds) : seconds(initialSeconds) {}
-
-    Timer(const string& timeString) {
-        parseTimeString(timeString);
-    }
-
-    Timer(int minutes, int seconds) : seconds(minutes * 60 + seconds) {}
-
-    // Функция для запуска таймера
-    void run() {
-        while (seconds > 0) {
-            displayTime();
-            sleep(1);  // Задержка на 1 секунду
-            --seconds;
-        }
-        cout << "\aTime's up! Ding ding ding!" << endl;  // Звонок
-    }
-
-private:
-    // Функция для отображения времени
-    void displayTime() const {
-        int minutes = seconds / 60;
-        int remainingSeconds = seconds % 60;
-        cout << setw(2) << setfill('0') << minutes << ":"
-            << setw(2) << setfill('0') << remainingSeconds << "\r" << flush;
-    }
-
-    // Функция для парсинга времени из строки в формате "mm:ss"
-    void parseTimeString(const string& timeString) {
-        size_t colonPos = timeString.find(':');
-        if (colonPos != string::npos) {
-            char* end;
-            long minutes = strtol(timeString.c_str(), &end, 10);
-            if (*end == ':' && end != timeString.c_str()) {
-                // Преобразование минут успешно, продолжаем с секундами
-                long seconds = strtol(end + 1, &end, 10);
-                if (*end == '\0') {
-                    // Преобразование секунд успешно, устанавливаем значение таймера
-                    this->seconds = minutes * 60 + seconds;
-                    return;
-                }
-            }
-        }
-
-        // Обработка ошибки
-        cerr << "Invalid time string format. Use \"mm:ss\"." << endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // Функция для создания задержки
-    void sleep(int seconds) const {
-        clock_t delay = seconds * CLOCKS_PER_SEC;
-        clock_t start = clock();
-        while ((clock() - start) < delay) {}
-    }
-};
-
-int main() {
-    // Примеры использования конструкторов
-    Timer timer1(10);  // Таймер на 3 минуты
-    Timer timer2("05:30");  // Таймер на 5 минут 30 секунд
-    Timer timer3(2, 30);  // Таймер на 2 минуты 30 секунд
-
-    // Запуск таймеров
-    cout << "Timer 1: ";
-    timer1.run();
-
-    cout << "Timer 2: ";
-    timer2.run();
-
-    cout << "Timer 3: ";
-    timer3.run();
-
-    return 0;
-}
+//#include <iostream>
+//#include <ctime>
+//#include <cstdlib>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//class Timer {
+//private:
+//    int seconds;
+//
+//public:
+//    // Конструкторы
+//    Timer(int initialSeconds) : seconds(initialSeconds) {}
+//
+//    Timer(const string& timeString) {
+//        parseTimeString(timeString);
+//    }
+//
+//    Timer(int minutes, int seconds) : seconds(minutes * 60 + seconds) {}
+//
+//    // Функция для запуска таймера
+//    void run() {
+//        while (seconds > 0) {
+//            displayTime();
+//            sleep(1);  // Задержка на 1 секунду
+//            --seconds;
+//        }
+//        cout << "\aTime's up! Ding ding ding!" << endl;  // Звонок
+//    }
+//
+//private:
+//    // Функция для отображения времени
+//    void displayTime() const {
+//        int minutes = seconds / 60;
+//        int remainingSeconds = seconds % 60;
+//        cout << setw(2) << setfill('0') << minutes << ":"
+//            << setw(2) << setfill('0') << remainingSeconds << "\r" << flush;
+//    }
+//
+//    // Функция для парсинга времени из строки в формате "mm:ss"
+//    void parseTimeString(const string& timeString) {
+//        size_t colonPos = timeString.find(':');
+//        if (colonPos != string::npos) {
+//            char* end;
+//            long minutes = strtol(timeString.c_str(), &end, 10);
+//            if (*end == ':' && end != timeString.c_str()) {
+//                // Преобразование минут успешно, продолжаем с секундами
+//                long seconds = strtol(end + 1, &end, 10);
+//                if (*end == '\0') {
+//                    // Преобразование секунд успешно, устанавливаем значение таймера
+//                    this->seconds = minutes * 60 + seconds;
+//                    return;
+//                }
+//            }
+//        }
+//
+//        // Обработка ошибки
+//        cerr << "Invalid time string format. Use \"mm:ss\"." << endl;
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    // Функция для создания задержки
+//    void sleep(int seconds) const {
+//        clock_t delay = seconds * CLOCKS_PER_SEC;
+//        clock_t start = clock();
+//        while ((clock() - start) < delay) {}
+//    }
+//};
+//
+//int main() {
+//    // Примеры использования конструкторов
+//    Timer timer1(10);  // Таймер на 3 минуты
+//    Timer timer2("05:30");  // Таймер на 5 минут 30 секунд
+//    Timer timer3(2, 30);  // Таймер на 2 минуты 30 секунд
+//
+//    // Запуск таймеров
+//    cout << "Timer 1: ";
+//    timer1.run();
+//
+//    cout << "Timer 2: ";
+//    timer2.run();
+//
+//    cout << "Timer 3: ";
+//    timer3.run();
+//
+//    return 0;
+//}
 
 //// 2 задание
 //#include <iostream>
@@ -147,58 +147,66 @@ int main() {
 //}
 
 // 3 задание
-//#include <iostream>
-//#include <string>
-//#include <ctime>
-//
-//class Soft {
-//private:
-//    std::string programName;
-//    std::string developer;
-//    double diskSpace;
-//    time_t licenseExpirationDate;
-//
-//public:
-//    // Конструктор по умолчанию
-//    Soft() : programName(""), developer(""), diskSpace(0.0), licenseExpirationDate(0) {}
-//
-//    // Конструктор с параметрами
-//    Soft(const std::string& name, const std::string& dev, double space, time_t expirationDate)
-//        : programName(name), developer(dev), diskSpace(space), licenseExpirationDate(expirationDate) {}
-//
-//    // Деструктор
-//    ~Soft() {
-//        // Здесь можно добавить код для освобождения ресурсов, если это необходимо
-//    }
-//
-//    // Функция для подсчета количества дней до завершения лицензии
-//    int daysUntilLicenseExpires() const {
-//        time_t currentTime;
-//        time(&currentTime);
-//        double secondsRemaining = difftime(licenseExpirationDate, currentTime);
-//        return static_cast<int>(secondsRemaining / (60 * 60 * 24));
-//    }
-//
-//    // Функция для вывода всех данных о программном обеспечении на экран
-//    void displaySoftwareInfo() const {
-//        std::cout << "Program Name: " << programName << std::endl;
-//        std::cout << "Developer: " << developer << std::endl;
-//        std::cout << "Disk Space: " << diskSpace << " GB" << std::endl;
-//        std::cout << "Days until License Expiration: " << daysUntilLicenseExpires() << " days" << std::endl;
-//    }
-//};
-//
-//int main() {
-//    // Пример использования класса Soft
-//    time_t expirationDate;
-//    time(&expirationDate);
-//    expirationDate += 30 * 24 * 60 * 60;  // Добавим 30 дней к текущей дате
-//
-//    Soft software1("MyApp", "MyCompany", 2.5, expirationDate);
-//
-//    // Вывод информации о программном обеспечении
-//    std::cout << "Software 1 Information:" << std::endl;
-//    software1.displaySoftwareInfo();
-//
-//    return 0;
-//}
+#include <iostream>
+#include <string>
+#include <chrono> // Для работы с временем
+#include <ctime>  // Для работы с временем
+
+class Soft {
+private:
+    std::string name;       // Название программы
+    std::string developer;  // Разработчик
+    double size;            // Занимаемый объем
+    std::tm expiryDate;     // Дата завершения лицензии
+
+public:
+    // Конструктор с параметрами
+    Soft(const std::string& _name, const std::string& _developer, double _size, const std::tm& _expiryDate)
+        : name(_name), developer(_developer), size(_size), expiryDate(_expiryDate) {}
+
+    // Деструктор
+    ~Soft() {
+        std::cout << "Destructor called for software: " << name << std::endl;
+    }
+
+    // Функция для подсчета количества дней до завершения лицензии
+    int daysUntilExpiry() const {
+        // Получаем текущую дату
+        std::time_t now = std::time(nullptr);
+        std::tm currentTime;
+        localtime_s(&currentTime, &now);
+
+        // Преобразуем даты в std::chrono::system_clock::time_point
+        std::tm expiry = expiryDate;
+        std::time_t t_expiry = std::mktime(&expiry);
+        std::time_t t_current = std::mktime(&currentTime);
+
+        // Разница в секундах между текущей датой и датой завершения лицензии
+        double difference = std::difftime(t_expiry, t_current);
+        // Переводим секунды в дни
+        return static_cast<int>(difference / (60 * 60 * 24));
+    }
+
+    // Функция для вывода всех данных на экран
+    void printData() const {
+        std::cout << "Name: " << name << std::endl;
+        std::cout << "Developer: " << developer << std::endl;
+        std::cout << "Size: " << size << " MB" << std::endl;
+        std::cout << "Expiry Date: " << expiryDate.tm_mday << "/" << expiryDate.tm_mon + 1 << "/" << expiryDate.tm_year + 1900 << std::endl;
+    }
+};
+
+int main() {
+    // Создание объекта класса Soft
+    std::tm expiryDate = { 0, 0, 0, 15, 1, 124 }; // Пример даты: 10/2/2024
+    Soft software("Example Software", "Example Developer", 100.5, expiryDate);
+
+    // Вывод данных об установленном программном обеспечении
+    software.printData();
+
+    // Подсчет количества дней до завершения лицензии
+    std::cout << "Days until expiry: " << software.daysUntilExpiry() << std::endl;
+
+    return 0;
+}
+
---
+++ Lab5OOP/Lab5OOP/Lab5OOP.cpp
@@ -0,0 +1,213 @@
+﻿//#include <iostream>
+//#include <limits>
+//#include <stdexcept>
+//
+//class Int {
+//private:
+//    int value;
+//
+//public:
+//    Int(int val = 0) : value(val) {}
+//
+//    Int operator+(const Int& other) const {
+//        long double result = static_cast<long double>(value) + other.value;
+//        if (result > std::numeric_limits<int>::max() || result < std::numeric_limits<int>::min()) {
+//            throw std::overflow_error("Arithmetic overflow occurred.");
+//        }
+//
+//        return Int(value + other.value);
+//    }
+//
+//    Int operator-(const Int& other) const {
+//        long double result = static_cast<long double>(value) - other.value;
+//        if (result > std::numeric_limits<int>::max() || result < std::numeric_limits<int>::min()) {
+//            throw std::overflow_error("Arithmetic overflow occurred.");
+//        }
+//        return Int(value - other.value);
+//    }
+//
+//    Int operator*(const Int& other) const {
+//        long double result = static_cast<long double>(value) * other.value;
+//        if (result > std::numeric_limits<int>::max() || result < std::numeric_limits<int>::min()) {
+//            throw std::overflow_error("Arithmetic overflow occurred.");
+//        }
+//        return Int(value * other.value);
+//    }
+//
+//    Int operator/(const Int& other) const {
+//        if (other.value == 0) {
+//            throw std::invalid_argument("Division by zero.");
+//        }
+//        return Int(value / other.value);
+//    }
+//
+//    Int& operator++() {
+//        if (value == std::numeric_limits<int>::max()) {
+//            throw std::overflow_error("Arithmetic overflow occurred.");
+//        }
+//        ++value;
+//        return *this;
+//    }
+//
+//    Int operator++(int) {
+//        Int temp(*this);
+//        ++(*this);
+//        return temp;
+//    }
+//
+//    friend std::ostream& operator<<(std::ostream& out, const Int& obj) {
+//        out << obj.value;
+//        return out;
+//    }
+//};
+//
+//int main() {
+//    // Пример использования класса Int
+//    Int a(2), b(10), c;
+//    try {
+//        c = a + b  ;
+//        std::cout << "a + b = " << c << std::endl;
+//
+//        // Проверка арифметического переполнения
+//        //c = Int(std::numeric_limits<int>::max()) + Int(1); // Вызовет исключение
+//    }
+//    catch (const std::exception& e) {
+//        std::cerr << "Exception caught: " << e.what() << std::endl;
+//    }
+//
+//    return 0;
+//}
+
+
+
+//////////////////////////////////////////////////////////////////////
+//
+//#include <iostream>
+//
+//class Fraction {
+//private:
+//    double chislo;
+//
+//public:
+//
+//    double get() const {
+//        return this->chislo;
+//    }
+//
+//    Fraction() : chislo(0.0) {}
+//    Fraction(double num) : chislo(num) {}
+//
+//    // Binary arithmetic operators
+//    Fraction operator+(const Fraction& other) const {
+//        return Fraction(chislo + other.chislo);
+//    }
+//
+//    Fraction operator-(const Fraction& other) const {
+//        return Fraction(chislo - other.chislo);
+//    }
+//
+//    Fraction operator*(const Fraction& other) const {
+//        return Fraction(chislo * other.chislo);
+//    }
+//
+//    Fraction operator/(const Fraction& other) const {
+//        if (other.chislo == 0.0) {
+//            throw std::invalid_argument("Division by zero");
+//        }
+//        return Fraction(chislo / other.chislo);
+//    }
+//
+//    // Prefix increment operator
+//    Fraction& operator++() {
+//        ++chislo;
+//        return *this;
+//    }
+//
+//    // Postfix increment operator
+//    Fraction operator++(int) {
+//        Fraction temp = *this;
+//        ++(*this);
+//        return temp;
+//    }
+//
+//    void display() const {
+//        std::cout << "Value: " << chislo << std::endl;
+//    }
+//};
+//
+//Fraction operator+( double a ,const Fraction& other)  {
+//    return Fraction( a+other.get());
+//}
+//
+//Fraction operator-(double a, const Fraction& other) {
+//    return Fraction(a - other.get());
+//}
+//
+//Fraction operator/(double a, const Fraction& other) {
+//    if (other.get() == 0.0) {
+//        throw std::invalid_argument("Division by zero");
+//    }
+//    return Fraction(a / other.get());
+//}
+//
+//Fraction operator*(double a, const Fraction& other) {
+//    return Fraction(a * other.get());
+//}
+//
+//int main() {
+//    Fraction a(0.5);
+//    Fraction b(0.25);
+//
+//    Fraction c = 2 + a + b + 2;
+//    c.display();
+//
+//    Fraction d = a - b;
+//    d.display();
+//
+//    Fraction e = a * b;
+//    e.display();
+//
+//    Fraction f = a / b;
+//    f.display();
+//
+//    ++a;
+//    a.display();
+//
+//    a++;
+//    a.display();
+//
+//    return 0;
+//}
+
+
+////////////////////////////
+
+#include <iostream>
+
+class Rectangle {
+private:
+    double length;
+    double width;
+
+public:
+    Rectangle(double l, double w) : length(l), width(w) {}
+
+    double rect_area() const {
+        return length * width;
+    }
+
+    // Overloaded rect_area() method for square
+    double rect_area(double side) const {
+        return side * side;
+    }
+};
+
+int main() {
+    Rectangle rectangle(3.0, 4.0);
+    std::cout << "Rectangle area: " << rectangle.rect_area() << std::endl;
+
+    // For square
+    std::cout << "Square area: " << rectangle.rect_area(5.0) << std::endl;
+
+    return 0;
+}
